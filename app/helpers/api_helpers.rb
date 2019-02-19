b040afa4a989e3d5d809d7c3777173f881422985
Gabriel Osborne <osbornegabriel@gmail.com>
2019-02-19 14:41:46 -0500

    Write rain? method

--- app/helpers/api_helpers.rb
+++ app/helpers/api_helpers.rb
@@ -6,11 +6,17 @@ helpers do
 
   def weather_call(zipcode)
     route_address = "http://api.openweathermap.org/data/2.5/forecast?zip=#{zipcode},us&APPID=#{ENV['WEATHERMAP_API']}"
-    p "Inside weather call"
     response = HTTParty.get(route_address, format: :plain)
     parsed = JSON.parse(response, symbolize_names: true)
-    description = parsed[:list][0][:weather][0][:description]
-    p description
+    weather_code = parsed[:list][0][:weather][0][:id]
+    p weather_code
+    p rain?(weather_code)
+  end
+
+  def rain?(forecast)
+    # Thunderstorm codes are listed as 200s, drizzling 300s, and rain 500s
+    # (There is no 400 id code)
+    forecast >= 200 && forecast < 600
   end
 
 end
