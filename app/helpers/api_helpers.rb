bd6a22d48e0f1055f2bde0bd774332401f549995
Gabriel Osborne <osbornegabriel@gmail.com>
2019-03-05 13:02:47 -0500

    Use weather data instead of forecast - today instead of next 5 days info

--- app/helpers/api_helpers.rb
+++ app/helpers/api_helpers.rb
@@ -5,19 +5,19 @@ helpers do
   # include HTTParty
 
   def zip_weather_call(zipcode)
-    route_address = "http://api.openweathermap.org/data/2.5/forecast?zip=#{zipcode},us&APPID=#{ENV['WEATHERMAP_API']}"
+    route_address = "http://api.openweathermap.org/data/2.5/weather?zip=#{zipcode},us&APPID=#{ENV['WEATHERMAP_API']}"
     weather_call(route_address)
   end
 
   def gps_weather_call(longitude,latitude)
-    route_address = "http://api.openweathermap.org/data/2.5/forecast?lat=#{latitude}&lon=#{longitude}&APPID=#{ENV['WEATHERMAP_API']}"
+    route_address = "http://api.openweathermap.org/data/2.5/weather?lat=#{latitude}&lon=#{longitude}&APPID=#{ENV['WEATHERMAP_API']}"
     weather_call(route_address)
   end
 
   def weather_call(route_address)
     response = HTTParty.get(route_address, format: :plain)
     parsed = JSON.parse(response, symbolize_names: true)
-    weather_code = parsed[:list][0][:weather][0][:id]
+    weather_code = parsed[:weather][0][:id]
     rain?(weather_code)
   end
 
