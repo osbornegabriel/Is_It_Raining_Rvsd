c247a20a32dab715008fa93d0d342c0104e541a9
Gabriel Osborne <osbornegabriel@gmail.com>
2019-03-05 12:46:02 -0500

    Write helper script for querying orientation

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -7,6 +7,11 @@ var Helpers = function(){
     $('#main-content').text('');
   }
 
+  // Checks @media orientation, for use when updating the background image
+  this.isLandscape = function(){
+    return window.matchMedia("(orientation: landscape)").matches;
+  }
+
   this.updateContent = function(txt){
     $('#main-content').html("<h1 class='cloud-text'>" + txt + "</h1>");
   }
